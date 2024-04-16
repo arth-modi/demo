8e4751dd8d3fe8b3b68524ca5e600525850883bb
arth-modi <arth.modi@brainvire.com>
2024-04-16 11:40:35 +0530

    id

--- employee/emp_register/models.py
+++ employee/emp_register/models.py
@@ -9,6 +9,6 @@ class Position(models.Model):
 
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
-    emp_code = models.CharField(max_length=3)
+    emp_code = models.CharField(max_length=4)
     mobile = models.CharField(max_length=15)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
