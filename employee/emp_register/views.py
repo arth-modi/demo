e11a2609a1749c51b7dcf9152005031834dfae04
arth-modi <arth.modi@brainvire.com>
2024-04-17 18:58:17 +0530

    cache edit

--- employee/emp_register/views.py
+++ employee/emp_register/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .forms import EmployeeForm
 from .models import Employee
 from django.contrib.auth import get_user_model
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_control, cache_page
 
 # @cache_page(60 * 15)
 # Create your views here.
@@ -18,11 +18,14 @@ from django.views.decorators.cache import cache_page
 #     all_users = user_model.objects.all()
 #     return HttpResponse('<html><body><h1>{0} users.. cacheless</h1></body></html>'.format(len(all_users)))
 
-@cache_page(60 * 15)
+# @cache_control(public = True, max_age = 3000)
+# @cache_page(60 * 15)
 def employee_list(request):
     context = {'employee_list' : Employee.objects.all()}
     return render(request, "emp_register/employee_list.html", context)
-@cache_page(60 * 15)
+
+# @cache_control(public = True, max_age = 3000)
+# @cache_page(60 * 15)
 def employee_form(request,id=0):
     if request.method == "GET":
         if id==0:
@@ -40,7 +43,9 @@ def employee_form(request,id=0):
         if form.is_valid():
             form.save()
         return redirect('/list')
-@cache_page(60 * 15)
+
+# @cache_control(public = True, max_age = 3000)
+# @cache_page(60 * 15)
 def employee_delete(request,id):
     employee = Employee.objects.get(pk = id)
     employee.delete()
